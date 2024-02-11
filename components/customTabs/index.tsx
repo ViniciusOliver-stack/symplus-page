7a724ba74cd3ceaf2837ff3359e1dfbd5c57a65b
Vinicius Oliveira <vinicius.oliveira@melius.software>
2024-02-11 18:44:06 -0300

    feat: adjusting the layout and inserting icons.

--- components/customTabs/index.tsx
+++ components/customTabs/index.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 import { motion } from "framer-motion"
 import Image from "next/image"
 import { useState } from "react"
@@ -6,8 +8,6 @@ import {
   Carousel,
   CarouselContent,
   CarouselItem,
-  CarouselNext,
-  CarouselPrevious,
 } from "@/components/ui/carousel"
 
 interface Tab {
@@ -29,24 +29,45 @@ export function CustomTabs({ tabs }: TabsProps) {
 
   return (
     <div>
-      <div className="flex mb-4 border-b border-b-[#ccc]/15">
-        <Carousel className="w-full flex items-center justify-center">
-          <CarouselContent className="ml-8 lg:w-[1050px]">
+      <div className="mb-4 border-b border-b-[#ccc]/15 md:w-full">
+        <Carousel
+          className="md:hidden "
+          opts={{
+            align: "start",
+          }}
+        >
+          <CarouselContent className="pr-2 flex items-center gap-4 w-[150px] text-center">
             {tabs.map((tab, index) => (
               <CarouselItem
                 key={index}
                 onClick={() => handleTabClick(index)}
-                className={`border  px-4 py-2 mr-4 gap-2 hover:cursor-pointer pl-1 basis1/2 md:basis-1/6 lg:basis-1/6 ${
+                className={`pr-4 ${
                   activeTab === index
-                    ? "text-blue-500 border-b border-b-blue-500"
+                    ? "text-blue-500 border-b border-b-blue-500 "
                     : "text-white"
                 }`}
               >
-                {tab.text}
+                <p>{tab.text}</p>
               </CarouselItem>
             ))}
           </CarouselContent>
         </Carousel>
+
+        <div className="hidden md:flex md:gap-12 md:items-center md:justify-center">
+          {tabs.map((tab, index) => (
+            <button
+              key={index}
+              onClick={() => handleTabClick(index)}
+              className={`px-1 pb-2 gap-2 hover:cursor-pointer ${
+                activeTab === index
+                  ? "text-blue-500 border-b border-b-blue-500"
+                  : "text-white"
+              }`}
+            >
+              <p>{tab.text}</p>
+            </button>
+          ))}
+        </div>
       </div>
       <motion.div
         className="flex mb-8"
@@ -57,11 +78,10 @@ export function CustomTabs({ tabs }: TabsProps) {
       >
         <Image
           width={1500}
-          height={600}
-          // src={tabs[activeTab].imageUrl}
-          src="/example.png"
+          height={300}
+          src={tabs[activeTab].imageUrl}
           alt={tabs[activeTab].text}
-          className="object-cover w-full"
+          className="object-cover w-full max-h-[700px] rounded-3xl"
         />
       </motion.div>
     </div>
