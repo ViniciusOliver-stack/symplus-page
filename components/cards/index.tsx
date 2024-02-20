8a32dd50a2398e7e816779b4304b82a7a9344fc3
Vinicius Oliveira <vinicius.oliveira@melius.software>
2024-02-19 22:21:52 -0300

    feat: Animation and improvement of code structure

--- components/cards/index.tsx
+++ components/cards/index.tsx
@@ -1,92 +1,52 @@
+import { motion } from "framer-motion"
 import Image from "next/image"
+
+const mockCardInfo = [
+  {
+    title: "Entrega rápida",
+    description: 'Vivamus lobortis, urna non ultricies placerat, ex quam luctus sem sed aliquet quam erat ut odio. Pellentesque habitant morbi',
+    image: "/img-entrega.svg"
+  },
+  {
+    title: "Entrega rápida",
+    description: 'Vivamus lobortis, urna non ultricies placerat, ex quam luctus sem sed aliquet quam erat ut odio. Pellentesque habitant morbi',
+    image: "/img-entrega.svg"
+  },
+  {
+    title: "Entrega rápida",
+    description: 'Vivamus lobortis, urna non ultricies placerat, ex quam luctus sem sed aliquet quam erat ut odio. Pellentesque habitant morbi',
+    image: "/img-entrega.svg"
+  },
+  {
+    title: "Entrega rápida",
+    description: 'Vivamus lobortis, urna non ultricies placerat, ex quam luctus sem sed aliquet quam erat ut odio. Pellentesque habitant morbi',
+    image: "/img-entrega.svg"
+  },
+]
+
 export function Cards() {
   return (
-    <div className="flex justify-center w-full">
-      <div className=" grid grid-cols-2 gap-x-10">
-        <div className="flex bg-white my-5 rounded-xl items-center h-60 w-[750px]">
-          <div className=" flex flex-col space-y-2 p-10">
-            <h2 className="text-neutral-900 font-bold text-xl">
-              Entrega rápida
-            </h2>
-            <p className="text-neutral-900 w-[400px] text-base">
-              Vivamus lobortis, urna non ultricies placerat, ex quam luctus sem
-              sed aliquet quam erat ut odio. Pellentesque habitant morbi
-            </p>
+    <div className="w-[85%] m-auto flex flex-wrap gap-4 items-center justify-center">
+      {mockCardInfo.map((card, index) => (
+        <motion.div className="flex flex-col md:flex-row items-center w-full md:w-fit bg-white rounded-lg" key={index}
+        initial={{ opacity: 0, x: -100 }}
+        whileInView={{ opacity: 1, x: 0 }}
+        exit={{ opacity: 0, x: -100 }}
+        transition={{ duration: 0.50, delay: index * 0.1 }}>
+          <div className="flex flex-col p-5 md:p-10 order-2 md:order-1">
+            <h2 className="text-neutral-900 font-bold text-xl">{card.title}</h2>
+            <p className="text-neutral-900 w-[300px]">{card.description}</p>
           </div>
-          <div className="">
+          <div className="order-1 md:order-1 md:mr-8">
             <Image
-              src="/img-entrega.svg"
+              src={card.image}
               alt=""
-              width={300}
-              height={300}
-              className=""
+              width={250}
+              height={250}
             />
           </div>
-        </div>
-
-        <div className="flex bg-white my-5 rounded-xl items-center h-60 w-[750px]">
-          <div className=" flex  flex-col space-y-2 p-10">
-            <h2 className="text-neutral-900 font-bold text-xl">
-              Suporte ao cliente
-            </h2>
-            <p className="text-neutral-900 w-[400px] text-base">
-              Vivamus lobortis, urna non ultricies placerat, ex quam luctus sem
-              sed aliquet quam erat ut odio. Pellentesque habitant morbi
-            </p>
-          </div>
-          <div className="">
-            <Image
-              src="/img-entrega.svg"
-              alt=""
-              width={300}
-              height={300}
-              className=""
-            />
-          </div>
-        </div>
-
-        <div className="flex bg-white my-5 rounded-xl items-center h-60 w-[750px]">
-          <div className=" flex  flex-col space-y-2 p-10">
-            <h2 className="text-neutral-900 font-bold text-xl">
-              Garantia de serviço
-            </h2>
-            <p className="text-neutral-900 w-[400px] text-base">
-              Vivamus lobortis, urna non ultricies placerat, ex quam luctus sem
-              sed aliquet quam erat ut odio. Pellentesque habitant morbi
-            </p>
-          </div>
-          <div className="">
-            <Image
-              src="/img-entrega.svg"
-              alt=""
-              width={300}
-              height={300}
-              className=""
-            />
-          </div>
-        </div>
-
-        <div className="flex bg-white my-5 rounded-xl items-center h-60 w-[750px]">
-          <div className=" flex  flex-col space-y-2 p-10">
-            <h2 className="text-neutral-900 font-bold text-xl">
-              Preço acessível
-            </h2>
-            <p className="text-neutral-900 w-[400px] text-base">
-              Vivamus lobortis, urna non ultricies placerat, ex quam luctus sem
-              sed aliquet quam erat ut odio. Pellentesque habitant morbi
-            </p>
-          </div>
-          <div className="">
-            <Image
-              src="/img-entrega.svg"
-              alt=""
-              width={300}
-              height={300}
-              className=""
-            />
-          </div>
-        </div>
-      </div>
+        </motion.div>
+      ))}
     </div>
   )
 }
--- components/tabsProducts/index.tsx
+++ components/tabsProducts/index.tsx
@@ -26,7 +26,6 @@ export function TabsProducts() {
 
       <div>
         <ScrollEffect />
-
       </div>
     </div>
   )
