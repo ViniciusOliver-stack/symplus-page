536e628d652abe835b74858d981576dec65bff1a
Vinicius Oliveira <vinicius.oliveira@melius.software>
2024-02-23 22:22:04 -0300

    feat: use component titleInformation

--- components/tabsProducts/index.tsx
+++ components/tabsProducts/index.tsx
@@ -1,28 +1,27 @@
-import ScrollEffect from "../scrollProducts";
+import ScrollEffect from "../scrollProducts"
+import { TitleInformation } from "../titleInformation"
 
 export function TabsProducts() {
-
   const content = [
     {
       title: "Título 1",
-      description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. In, id voluptatum cum illum quod asperiores, quaerat porro vel eaque, inventore accusamus veritatis? Magni exercitationem aspernatur quas quam. Sunt, obcaecati? Nisi!",
-
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. In, id voluptatum cum illum quod asperiores, quaerat porro vel eaque, inventore accusamus veritatis? Magni exercitationem aspernatur quas quam. Sunt, obcaecati? Nisi!",
     },
     {
       title: "Título 2",
-      description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. In, id voluptatum cum illum quod asperiores, quaerat porro vel eaque, inventore accusamus veritatis? Magni exercitationem aspernatur quas quam. Sunt, obcaecati? Nisi!",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. In, id voluptatum cum illum quod asperiores, quaerat porro vel eaque, inventore accusamus veritatis? Magni exercitationem aspernatur quas quam. Sunt, obcaecati? Nisi!",
     },
     // Adicione mais itens conforme necessário
-  ];
-
-
+  ]
 
   return (
-    <div className="w-[85%] m-auto mt-6 md:mt-32">
-      <div className="w-[85%] m-auto text-center flex flex-col gap-4">
-        <h2 className="text-4xl md:text-[40px]">Veja nossos produtos</h2>
-        <p>Veja os produtos que atendem a sua necessidade.</p>
-      </div>
+    <div className="w-[85%] m-auto">
+      <TitleInformation
+        title="Veja nosso produtos"
+        description="Conheça alguns de nossos produto que facilitam e atendem a sua necessidade e da sua empresa."
+      />
 
       <div>
         <ScrollEffect />
