ad07073f474f1af48c370e057888e032dbd48023
Vinicius Oliveira <vinicius.oliveira@melius.software>
2024-03-02 11:17:18 -0300

    feat: component newsletter

--- components/newsletter/index.tsx
+++ components/newsletter/index.tsx
@@ -1,42 +1,59 @@
-import Image from "next/image";
+import Image from "next/image"
+import { InputFocusBlur } from "../inputFocusBlur"
+import { Meteors } from "../ui/meteors"
 
 export function Newsletter() {
   return (
-    <div className="flex justify-around px-64">
-      <div className="pt-24">
-        <h1 className="text-4xl font-bold">Assine nossa NewsLetter</h1>
-        <Image
-          className=" ml-56"
-          src="/Underline-newsletter.svg"
-          alt=""
-          width={300}
-          height={30}
-        />
-        <p className="text-base w-1/2 pt-28">
-          Não perca as últimas novidades em tecnologia e notícias exclusivas da
-          nossa empresa! Assine nossa newsletter agora e mantenha-se à frente do
-          jogo. Seja o primeiro a receber insights, atualizações e ofertas
-          especiais diretamente na sua caixa de entrada. Junte-se à nossa
-          comunidade de entusiastas de tecnologia hoje mesmo!
-        </p>
+    <div className="w-[85%] md:w-[60%] m-auto select-none ">
+      <div className="">
+        <div className=" inset-0 transform scale-[0.80] rounded-full blur-3xl" />
+        <div className=" shadow-xl bg-gray-900 border border-gray-800  px-4 py-8 overflow-hidden rounded-2xl flex flex-col justify-end items-start">
+          <div className="md:grid md:grid-cols-2 items-center gap-10">
+            <div className="flex flex-col gap-4 text-center">
+              <p className="text-4xl font-semibold">
+                Assine nossa
+                <span className="inline-block ml-2.5">
+                  newsletter
+                  <Image
+                    src="/Underline-newsletter.svg"
+                    alt=""
+                    width={300}
+                    height={30}
+                  />
+                </span>
+              </p>
+              <p>
+                Não perca as últimas novidades em tecnologia e notícias
+                exclusivas da nossa empresa! Assine nossa newsletter agora e
+                mantenha-se à frente do jogo. Seja o primeiro a receber
+                insights, atualizações e ofertas especiais diretamente na sua
+                caixa de entrada. Junte-se à nossa comunidade de entusiastas de
+                tecnologia hoje mesmo!
+              </p>
+            </div>
+            <div className="flex flex-col gap-4 mt-8 md:mt-8">
+              <InputFocusBlur placeholder="Digite o seu email aqui" />
+              <button
+                className='isolate px-4 py-2 rounded-xl bg-transparent shadow-lg ring-1 ring-black/8 relative z-0 overflow-hidden whitespace-nowrap font-medium
+            text-neutral-300 transition-all duration-300
+              before:absolute before:inset-0
+              before:-z-10 before:translate-y-[200%]
+              before:scale-[2.5]
+              before:rounded-[100%] before:bg-neutral-50
+              before:transition-transform before:duration-1000
+              before:content-[""]
+              hover:scale-105 hover:text-neutral-900
+              hover:before:translate-y-[0%]
+              active:scale-100'
+              >
+                Assine agora!
+              </button>
+            </div>
+          </div>
+
+          {/* <Meteors number={20} /> */}
+        </div>
       </div>
-      <div className="place-content-end">
-        <input
-          className="h-20 w-[36rem] text-black text-xl font-medium p-1 pl-6 rounded-xl bg-white mt-60 placeholder-blue-600 placeholder-opacity-85 placeholder:text-xl placeholder:font-medium outline-none"
-          type="email"
-          placeholder="Seu e-mail aqui"
-        />
-        <button className=" bg-slate-400 px-12 py-3 mt-6 ml-[21rem] opacity-90 rounded-xl font-medium text-white text-lg">
-          ASSINE AGORA!
-        </button>
-      </div>
-      <Image
-        src="/background-newsletter.svg"
-        alt=""
-        width={1620}
-        height={450}
-        className="-z-50 absolute"
-      />
     </div>
-  );
+  )
 }
