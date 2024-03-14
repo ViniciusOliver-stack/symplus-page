f548a4bc5c30ae7b1d633ba155fbfca2b2d44663
ViniciusOliver-stack <vinicius.so.contato@hotmail.com>
2024-03-14 08:51:30 -0300

    feat: layout and text adjustment

--- components/formulary/index.tsx
+++ components/formulary/index.tsx
@@ -3,34 +3,23 @@ import { Formulario } from "../ui/inputForm"
 
 export function Formulary() {
   return (
-    <div className="w-[85%] m-auto scroll-section" id="contact">
-      <div className="flex flex-col md:flex-row gap-6 flex-wrap items-center justify-between">
+    <div className="w-[85%] m-auto scroll-section mt-20" id="contact">
+      <div className="flex items-center justify-around flex-col md:flex-row gap-6 flex-wrap">
         <div>
-          <Image
-            className="-z-50 absolute -left-44 mt-24"
-            src="/Rectangle 45.svg"
-            alt=""
-            width={600}
-            height={500}
-          />
           <div className="flex flex-col gap-4">
+            <p className="uppercase font-light text-lg">Fale Conosco</p>
             <h3 className="text-5xl md:w-[550px]">
               Vamos levar sua marca ao próximo
               <span className="text-blue-700 ml-1.5">nível</span>.
             </h3>
-            <p className="text-lg text-neutral-500">
-              Você pode entrar em contato conosco a qualquer momento <br /> via
-              <span className="text-blue-700 ml-1.5">contato@symplus.net</span>
+            <p className="text-lg text-neutral-500 max-w-[450px]">
+              Sabemos que provavelmente você tem uma rotina corrida, mas em{" "}
+              <span className="text-white">40 minutos</span>, conseguimos
+              apresentar tudo aquilo que é relevante para você começar a
+              alcançar o <span className="text-white">próximo nível</span>.{" "}
+              <br />
             </p>
           </div>
-
-          <Image
-            className="hidden md:block"
-            src="/Revenue-bro 1.svg"
-            alt=""
-            width={400}
-            height={400}
-          />
         </div>
 
         <Formulario />
