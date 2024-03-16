f6a8bbe99e1bcbbbe675df7a14b884e7682d8fdc
ViniciusOliver-stack <vinicius.so.contato@hotmail.com>
2024-03-16 11:01:58 -0300

    feat: add comment

--- components/infiniteMovingCardsDemo/index.tsx
+++ components/infiniteMovingCardsDemo/index.tsx
@@ -28,32 +28,32 @@ export function InfiniteMovingCardsDemo() {
 const testimonials = [
   {
     quote:
-      "It was the best of times, it was the worst of times, it was the age of wisdom, it was the age of foolishness, it was the epoch of belief, it was the epoch of incredulity, it was the season of Light, it was the season of Darkness, it was the spring of hope, it was the winter of despair.",
-    name: "Vinicius",
-    title: "CEO | Symplus",
+      "Antes da Symplus, meu Instagram estava estagnado. Com seu serviço de emulsionamento e produção de posts, vi um aumento significativo de seguidores e engajamento. Agora, recebo elogios pela qualidade do conteúdo e vejo mais clientes e rentabilidade. A equipe entendeu minha visão e entregou resultados excepcionais. Recomendo a Symplus para quem quer impulsionar sua presença no Instagram.",
+    name: "Lucas",
+    title: "CEO | Barbearia Saluc",
   },
   {
     quote:
-      "To be, or not to be, that is the question: Whether 'tis nobler in the mind to suffer The slings and arrows of outrageous fortune, Or to take Arms against a Sea of troubles, And by opposing end them: to die, to sleep.",
-    name: "Tarcísio",
-    title: "CEO | Symplus",
+      "A aplicação web da Big Lanches mudou completamente a forma como os clientes pedem seus lanches favoritos. Agora, podem visualizar o cardápio facilmente, fazer seu pedido pedido em poucos cliques e esperar meu lanche chegar fresquinho. É rápido, conveniente e tornou a experiência de pedir comida ainda melhor. Recomendo totalmente!",
+    name: "Lara",
+    title: "CEO | Big Lanches",
   },
   {
     quote:
-      "It was the best of times, it was the worst of times, it was the age of wisdom, it was the age of foolishness, it was the epoch of belief, it was the epoch of incredulity, it was the season of Light, it was the season of Darkness, it was the spring of hope, it was the winter of despair.",
-    name: "Thiago",
-    title: "CEO | Symplus",
+      "Com o tráfego pago e geração de leads da Symplus, vimos um aumento significativo em clientes potenciais. Eles não só direcionaram tráfego qualificado para nosso site, mas também nos forneceram leads valiosos que impulsionaram nossas vendas. Recomendo fortemente seus serviços.",
+    name: "João Silva",
+    title: "Diretor de Marketing | TechSoluções",
   },
   {
     quote:
-      "It was the best of times, it was the worst of times, it was the age of wisdom, it was the age of foolishness, it was the epoch of belief, it was the epoch of incredulity, it was the season of Light, it was the season of Darkness, it was the spring of hope, it was the winter of despair.",
-    name: "Caio",
-    title: "CEO | Symplus",
+      "A landing page criada pela Symplus foi fundamental para nossa empresa. Aumentamos nossa taxa de conversão e melhoramos a experiência do usuário, destacando nossos serviços de forma clara e atrativa. Obrigado, Symplus, por nos ajudar a alcançar nossos objetivos!",
+    name: "Ana Oliveira,",
+    title: "CEO | DigitalInovação",
   },
   {
     quote:
-      "Call me Ishmael. Some years ago—never mind how long precisely—having little or no money in my purse, and nothing particular to interest me on shore, I thought I would sail about a little and see the watery part of the world.",
-    name: "Luender",
-    title: "CEO | Symplus",
+      "A aplicação da Symplus resolveu todos os nossos problemas de gerenciamento de comércio. Desde controle de estoque até gestão de vendas, tudo se tornou mais simples e eficiente. É como ter um assistente virtual que cuida de todos os aspectos do nosso negócio. Estamos muito satisfeitos e gratos à equipe da Symplus pelo excelente trabalho!",
+    name: "Pedro Santos",
+    title: "Gerente de Operações | VendaFácil",
   },
 ]
